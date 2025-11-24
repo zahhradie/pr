d0964416c949b232dc3963488cecb7d060a5c60d
Zahra Farhadova <rehmanferhadli2016@gmail.com>
2025-11-24 21:41:20 +0400

    Create function1_5.cpp

---
+++ function1_5.cpp
@@ -0,0 +1,14 @@
+//selsini farenheyte
+#include <iostream>
+using namespace std;
+ float cevirme (int x)
+ {
+  float f;
+  f=x*9/5+32;
+  return f;
+}
+ int main(){
+     int y;
+     y= cevirme(20);
+     cout<<y;
+ }
